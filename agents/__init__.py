1918ae98349ad3bd42f6efe1df3835de36d8f806
madbulgarianfromukraine <104125216+madbulgarianfromukraine@users.noreply.github.com>
2026-01-04 14:28:28 +0100

    updating imports(were non-functional before)

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,10 +2,10 @@
 This file simplifies imports of the agents into the other modules.
 '''
 
-from anonymity_agent import anonymity_agent
-from formatting_agent import formatting_agent
-from policy_agent import policy_agent
-from safety_agent import safety_agent
-from scope_agent import scope_agent
-from visual_agent import visual_agent
-from final_decision_agent import final_decision_agent
+from .anonymity_agent import anonymity_agent
+from .formatting_agent import formatting_agent
+from .policy_agent import policy_agent
+from .safety_agent import safety_agent
+from .scope_agent import scope_agent
+from .visual_agent import visual_agent
+from .final_decision_agent import final_decision_agent
