9bf0d0207f118dfb945229acd5d2b9cd7e026a19
madbulgarianfromukraine <104125216+madbulgarianfromukraine@users.noreply.github.com>
2026-01-04 14:29:21 +0100

    changing the cache creation and finishing it.

--- core/cache_manager.py
+++ core/cache_manager.py
@@ -26,7 +26,7 @@ def get_optimized_fallback_mime(file_path: str) -> str:
         case _:
             return 'text/plain'  # Ultimate default for unknown binaries
 
-def upload_single_file(client: genai.Client, file_path: Optional[Path | str] = None) -> Optional[str]:
+def upload_single_file(client: genai.Client, file_path: Optional[Path | str] = None) -> Optional[genai.types.File]:
     try:
         file = client.files.upload(file=file_path,
                                    config=genai.types.UploadFileConfig(mime_type=get_optimized_fallback_mime(file_path)))
@@ -38,34 +38,16 @@ def upload_single_file(client: genai.Client, file_path: Optional[Path | str] = N
         print(f"Failed to upload the file {file_path} due to {e}")
         return None
 
-    return file.name
+    return file
 
-def create_paper_cache(
-        pdf_path: str,
-        style_guide_paths: List[str] = STYLE_GUIDES_DEFAULT,
-        supplemental_paths: List[str] = [],
-        model_name: str = "gemini-2.5-flash-lite"
-):
+def create_paper_cache(style_guide_paths: List[str] = STYLE_GUIDES_DEFAULT, model_name: str = "gemini-2.5-flash-lite"):
     print("--- Uploading Files to Gemini Cache ---")
     client = genai.Client()
-    # 1. Upload Main Paper
-    print(f"Uploading main paper: {pdf_path}...")
-    paper_file = upload_single_file(client, file_path=pdf_path)
-    if paper_file is None:
-        raise Exception("Have not been able to upload a main.pdf")
-
-    print(f'Uploading supplemental paths: {supplemental_paths}...')
-    supplemental_paths_uploaded = []
-    with ThreadPoolExecutor(max_workers=10, thread_name_prefix="SupplementalPathThread") as executor:
-        supplemental_paths_uploaded = list(executor.map(upload_single_file, [(client, supplemental_path) for supplemental_path in supplemental_paths]))
-
-    if len(supplemental_paths_uploaded) != len(supplemental_paths):
-        print("Have not been able to upload some supplemental files")
 
     print(f'Uploading style guide paths: {style_guide_paths}...')
     style_guide_paths_uploaded = []
     with ThreadPoolExecutor(max_workers=4, thread_name_prefix="StyleGuidePathThread") as executor:
-        style_guide_paths_uploaded = list(executor.map(upload_single_file, [(client, supplemental_path) for supplemental_path in supplemental_paths]))
+        style_guide_paths_uploaded = list(executor.map(upload_single_file, [(client, supplemental_path) for supplemental_path in style_guide_paths]))
 
     if len(style_guide_paths_uploaded) != len(style_guide_paths):
         print("Have not been able to upload some style guide files")
@@ -75,8 +57,7 @@ def create_paper_cache(
         genai.types.Content(
             role="user",
             parts=[
-                genai.types.Part.from_uri(file_uri=document1.uri, mime_type=document1.mime_type),
-                genai.types.Part.from_uri(file_uri=document2.uri, mime_type=document2.mime_type),
+                genai.types.Part.from_uri(file_uri=document.uri, mime_type=get_optimized_fallback_mime(document.uri)) for document in style_guide_paths_uploaded
             ]
         ),
     ]
